c6017aed44a0a56da6f220f0e0bfba4ffb8ba7e7
kirito22037 <kirito22037@gmail.com>
2021-11-19 11:40:17 +0530

    null safety example, remember to use the ? after variable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,11 @@ class Main {
 
 }
 
+// Null Safety handles the NullPointerException , so our code doesn't break in between
 fun main() {
-    val firstName: String = "Roshan"
-    val lastName: String = "Rai"
+    val firstName: String? = "Roshan"
+    val lastName: String? = null
 
-    println("My name is $firstName $lastName")
+    println("My first name length is ${firstName?.length}")
+    println("My last name length is ${lastName?.length}")
 }
