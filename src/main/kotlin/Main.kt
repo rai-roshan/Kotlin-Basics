59df1e892e831c0a55eed42720d5d508ebcbbbf4
kirito22037 <kirito22037@gmail.com>
2021-11-19 11:56:47 +0530

    alternative for ternary operator in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,21 +2,22 @@ class Main {
 
 }
 
-// alternate of switch case
+/* there is no terniary opertator in Kotlin
+* instead we can use if-else and when as expressions */
 fun main() {
+
+    val eligible: Boolean
+    val age: Int = 18
+    eligible = if(age<18) false else true
+
     val userType: String = "admin"
-    when( userType ) {
-        "admin" -> {
-            println("you are granted all the rights")
-        }
-        "user" -> {
-            println("restricted rights to user")
-        }
-        "group" -> {
-            println("for additional rights ask admin for permission")
-        }
-        else -> {
-            println("this case is not handled")
-        }
+    val rights: String = when( userType ) {
+        "admin" -> "you are granted all the rights"
+        "user" -> "restricted rights to user"
+        "group" -> "for additional rights ask admin for permission"
+        else -> "this case is not handled"
     }
+
+    println("is user eligible : $eligible")
+    println("users rights : $rights")
 }
